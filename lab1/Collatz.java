745fc6210cab7311f6e168f28d5a20e96177ad82
ypy233 <162784300+ypy233@users.noreply.github.com>
2024-04-19 23:03:29 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,14 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author ypy233
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 1) {
+            return n * 3 + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
@@ -24,4 +22,3 @@ public class Collatz {
         System.out.println();
     }
 }
-
